343ad001ba506853edd03def9359ad47c61166a2
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2023-11-18 23:14:36 +0000

    feat: Add Extension methods for `.Returns` of `Task<T>` and `ValueTask<T>` (#2)
    Adds a set of new Extension Methods for `.Returns` that are specifically
    tailored for `Task<T>`/`ValueTask<T>`.
    
    Using these extension methods means you can setup a return value without
    needing a `Task.FromResult(...)` keeping consuming code clean.

---
+++ src/Mimic/Extensions/ReturnsExtensions.cs
@@ -0,0 +1,52 @@
+﻿using JetBrains.Annotations;
+using Mimic.Setup.Fluent;
+
+namespace Mimic;
+
+[PublicAPI]
+public static partial class ReturnsExtensions
+{
+    #region Task<T>
+
+    public static IReturnsResult<TMimic> Returns<TMimic, TResult>(this IReturns<TMimic, Task<TResult>> mimic, TResult value)
+        where TMimic : class
+    {
+        return mimic.Returns(() => Task.FromResult(value));
+    }
+
+    public static IReturnsResult<TMimic> Returns<TMimic, TResult>(this IReturns<TMimic, Task<TResult>> mimic, Func<TResult> valueFunction)
+        where TMimic : class
+    {
+        return mimic.Returns(() => Task.FromResult(valueFunction()));
+    }
+
+    public static IReturnsResult<TMimic> Returns<TMimic, T, TResult>(this IReturns<TMimic, Task<TResult>> mimic, Func<T, TResult> valueFunction)
+        where TMimic : class
+    {
+        return mimic.Returns((T t) => Task.FromResult(valueFunction(t)));
+    }
+
+    #endregion
+
+    #region ValueTask<T>
+
+    public static IReturnsResult<TMimic> Returns<TMimic, TResult>(this IReturns<TMimic, ValueTask<TResult>> mimic, TResult value)
+        where TMimic : class
+    {
+        return mimic.Returns(() => new ValueTask<TResult>(value));
+    }
+
+    public static IReturnsResult<TMimic> Returns<TMimic, TResult>(this IReturns<TMimic, ValueTask<TResult>> mimic, Func<TResult> valueFunction)
+        where TMimic : class
+    {
+        return mimic.Returns(() => new ValueTask<TResult>(valueFunction()));
+    }
+
+    public static IReturnsResult<TMimic> Returns<TMimic, T, TResult>(this IReturns<TMimic, ValueTask<TResult>> mimic, Func<T, TResult> valueFunction)
+        where TMimic : class
+    {
+        return mimic.Returns((T t) => new ValueTask<TResult>(valueFunction(t)));
+    }
+
+    #endregion
+}
