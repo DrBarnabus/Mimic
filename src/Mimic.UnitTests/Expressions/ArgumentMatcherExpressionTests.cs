e11a25d12ce9dc6bd17eaa6ffad11e30861f055c
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2023-11-25 16:55:45 +0000

    refactor: Move `ArgumentMatcherExpression` into Expressions
    Also adds unit tests for the `ArgumentMatcherExpression` class.

---
+++ src/Mimic.UnitTests/Expressions/ArgumentMatcherExpressionTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+using Mimic.Expressions;
+using Mimic.Setup.ArgumentMatchers;
+
+namespace Mimic.UnitTests.Expressions;
+
+public class ArgumentMatcherExpressionTests
+{
+    [Fact]
+    public void Constructor_ShouldAppropriatelySetProperties()
+    {
+        var argumentMatcher = new ArgumentMatcher<string>(_ => true);
+
+        var expression = new ArgumentMatcherExpression(argumentMatcher);
+
+        expression.ShouldNotBeNull();
+        expression.ArgumentMatcher.ShouldBeSameAs(argumentMatcher);
+        expression.NodeType.ShouldBe(ExpressionType.Extension);
+        expression.Type.ShouldBe(argumentMatcher.Type);
+        expression.CanReduce.ShouldBeFalse();
+    }
+}
--- src/Mimic/Expressions/ArgumentMatcherExpression.cs
+++ src/Mimic/Expressions/ArgumentMatcherExpression.cs
@@ -1,16 +1,18 @@
-﻿namespace Mimic.Setup.ArgumentMatchers;
+﻿using Mimic.Setup.ArgumentMatchers;
+
+namespace Mimic.Expressions;
 
 internal sealed class ArgumentMatcherExpression : Expression
 {
     internal ArgumentMatcher ArgumentMatcher { get; }
-
-    public ArgumentMatcherExpression(ArgumentMatcher argumentMatcher) => ArgumentMatcher = argumentMatcher;
 
     public override ExpressionType NodeType => ExpressionType.Extension;
 
     public override Type Type => ArgumentMatcher.Type;
 
     public override bool CanReduce => false;
+
+    public ArgumentMatcherExpression(ArgumentMatcher argumentMatcher) => ArgumentMatcher = argumentMatcher;
 
     protected override Expression VisitChildren(ExpressionVisitor visitor) => this;
 }
